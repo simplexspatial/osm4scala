44b30a46863929c474d30bf4706ddd6cc619fd94
Angel Cervera Claudio <293444+angelcervera@users.noreply.github.com>
2021-03-27 16:57:20 +0000

    Remove not used links

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,17 +6,7 @@ module.exports = {
       items: [
         'overview',
         'spark-connector',
-        {
-          type: 'category',
-          label: 'Spark Connector',
-          items: [
-          ]
-        },
         'standalone-scala-library',
-        'create-a-page',
-        'create-a-document',
-        'markdown-features',
-        'thank-you',
       ],
     },
   ],
